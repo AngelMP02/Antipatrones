6847e62fd83116f23408b52f1027b174f249d41e
AngelMP02 <94680353+AngelMP02@users.noreply.github.com>
2023-11-24 17:32:56 +0100

    flask

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
     return render_template('calculator.html')
 
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    expression = request.form['expression']
-    try:
-        result = eval(expression)
-        return {'result': result}
-    except Exception as e:
-        return {'error': str(e)}
-
 if __name__ == '__main__':
     app.run(debug=True)
+
