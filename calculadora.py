0aea2e2d252a4fbd060dca8e3f44f9cc3826e640
AngelMP02 <94680353+AngelMP02@users.noreply.github.com>
2023-11-24 16:53:48 +0100

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,4 @@
+#creamos funciones para cada operacion
 def suma(num1, num2):
     return num1 + num2
 
@@ -11,4 +12,15 @@ def division(num1, num2):
     if num2 != 0:
         return num1 / num2
     else:
-        raise ValueError("No se puede dividir entre cero.")
+        raise ValueError("No se puede dividir entre cero.") 
+def calcular(operacion, num1, num2):
+    if operacion == 'suma':
+        return suma(num1, num2)
+    elif operacion == 'resta':
+        return resta(num1, num2)
+    elif operacion == 'multiplicacion':
+        return multiplicacion(num1, num2)
+    elif operacion == 'division':
+        return division(num1, num2)
+    else:
+        raise ValueError("Operación no soportada.")
